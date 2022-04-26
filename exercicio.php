abf498d39bffd9b6586dd847938a2c14e4d065b1
Wesleycnc <wesleycaldas728@gmail.com>
2022-04-26 16:00:55 -0300

    Create exercicio.php

---
+++ exercicio.php
@@ -0,0 +1,31 @@
+ <!DOCTYPE html>
+ <html lang="pt-br">
+ <head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Exercicio</title>
+ </head>
+ <body>
+     <h1>Exercicio</h1>
+     <hr>
+
+<?php
+ $data = "26/04/2022";
+ $nome = "Wesley";
+ $idade = 18;
+ $cidade = "São Paulo";
+
+ echo "<p> Hoje é $data. tem $idade anos e nasceu em $cidade </p>"; 
+ 
+
+ ?>   
+     
+ </body>
+ </html>
+ 
+ 
+ 
+ 
+ 
+
